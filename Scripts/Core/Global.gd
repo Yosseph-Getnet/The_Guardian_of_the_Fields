ed167cd51426c79d3d5b03ba4810868ecc1b571b
Yosseph-Getnet <Yosseph.07w@gmail.com>
2026-04-06 16:20:00 +0300

    Task Folder missing added

---
+++ Scripts/Core/Global.gd
@@ -0,0 +1,5 @@
+var current_ammo: int = 20
+var max_ammo: int = 50
+var herd_integrity: float = 100.0  # From SDD Phase 2
+var player_thirst: float = 100.0   # For Level 3 logic
+var boss_enraged: bool = false
