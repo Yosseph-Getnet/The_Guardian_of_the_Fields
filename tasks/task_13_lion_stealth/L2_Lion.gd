62ad8d70766ddd0b2f6f7b9dc048e611f3bd0a50
Amanti Wakshuma <amentiwakshuma@gmail.com>
2026-04-11 10:59:28 +0300

    Summary (required): S-13: Lion stealth + tall grass complete  Description (optional): - Lion sprite sheet with 4 frames (idle, crouch, leap, land) - Tall grass sprite with 2-frame sway - Lion inherits Base_Hazard - Opacity = 0.3 when in grass - Pounce triggers at <100px distance - lion_pounce_warning signal emits 1.5s before pounce

---
+++ tasks/task_13_lion_stealth/L2_Lion.gd
@@ -0,0 +1,51 @@
+extends "res://Scripts/Core/Entity.gd"
+
+signal lion_pounce_warning
+signal lion_pounced
+
+const STEALTH_OPACITY = 0.3
+const POUNCE_DISTANCE = 100
+const WARNING_TIME = 1.5
+
+var is_in_grass = false
+var is_pouncing = false
+var target_player = null
+
+func _ready():
+	modulate.a = 1.0
+
+func enter_grass():
+	is_in_grass = true
+	modulate.a = STEALTH_OPACITY
+
+func exit_grass():
+	is_in_grass = false
+	modulate.a = 1.0
+
+func set_target_player(player):
+	target_player = player
+
+func check_pounce():
+	if is_pouncing:
+		return
+	
+	if target_player and is_in_grass:
+		var distance = global_position.distance_to(target_player.global_position)
+		if distance < POUNCE_DISTANCE:
+			start_pounce()
+
+func start_pounce():
+	is_pouncing = true
+	
+	emit_signal("lion_pounce_warning")
+	await get_tree().create_timer(WARNING_TIME).timeout
+	
+	modulate.a = 1.0
+	emit_signal("lion_pounced")
+	
+	# TODO: Add pounce attack logic here
+	
+	is_pouncing = false
+
+func _process(delta):
+	check_pounce()
